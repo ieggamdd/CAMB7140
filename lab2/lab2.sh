077fc3c41b3a7f9cc40ed765b494f5df9aa3dad9
Maggie <ma.zhousj@gmail.com>
2024-02-09 22:06:25 -0500

    upload files

---
+++ lab2/lab2.sh
@@ -0,0 +1,29 @@
+#fastqc SRR8668755_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668755_10K_subsample_mapped --single -l 250 -s 30 SRR8668755_10K_subsample.fastq.gz -t 8 &> SRR8668755_10K_subsample_mapped.log
+#fastqc SRR8668756_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668756_10K_subsample_mapped --single -l 250 -s 30 SRR8668756_10K_subsample.fastq.gz -t 8 &> SRR8668756_10K_subsample_mapped.log
+#fastqc SRR8668757_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668757_10K_subsample_mapped --single -l 250 -s 30 SRR8668757_10K_subsample.fastq.gz -t 8 &> SRR8668757_10K_subsample_mapped.log
+#fastqc SRR8668758_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668758_10K_subsample_mapped --single -l 250 -s 30 SRR8668758_10K_subsample.fastq.gz -t 8 &> SRR8668758_10K_subsample_mapped.log
+#fastqc SRR8668759_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668759_10K_subsample_mapped --single -l 250 -s 30 SRR8668759_10K_subsample.fastq.gz -t 8 &> SRR8668759_10K_subsample_mapped.log
+#fastqc SRR8668769_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668769_10K_subsample_mapped --single -l 250 -s 30 SRR8668769_10K_subsample.fastq.gz -t 8 &> SRR8668769_10K_subsample_mapped.log
+#fastqc SRR8668771_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668771_10K_subsample_mapped --single -l 250 -s 30 SRR8668771_10K_subsample.fastq.gz -t 8 &> SRR8668771_10K_subsample_mapped.log
+#fastqc SRR8668772_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668772_10K_subsample_mapped --single -l 250 -s 30 SRR8668772_10K_subsample.fastq.gz -t 8 &> SRR8668772_10K_subsample_mapped.log
+#fastqc SRR8668773_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668773_10K_subsample_mapped --single -l 250 -s 30 SRR8668773_10K_subsample.fastq.gz -t 8 &> SRR8668773_10K_subsample_mapped.log
+#fastqc SRR8668774_10K_subsample.fastq.gz
+#kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o SRR8668774_10K_subsample_mapped --single -l 250 -s 30 SRR8668774_10K_subsample.fastq.gz -t 8 &> SRR8668774_10K_subsample_mapped.log
+
+for FASTQ in *fastq*
+do
+        OUT=${FASTQ//.fastq.gz/_mapped}
+        LOG=${FASTQ//.fastq.gz/_mapped.log}
+        echo "fastqc $FASTQ"
+        kallisto quant -i Homo_sapiens.GRCh38.cdna.all.index -o $OUT --single -l 250 -s 30 $FASTQ -t 8 &> $LOG" 
+done
+multiqc -d .
